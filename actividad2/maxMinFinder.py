baf14bb173362444eebbb006bdd5d4de78794882
marton2307 <marton2307@gmail.com>
2024-05-09 10:16:12 -0500

    check

--- actividad2/maxMinFinder.py
+++ actividad2/maxMinFinder.py
@@ -1,8 +1,8 @@
 def find_max_min(array):
-    if len(array) == 0:
+    if len(array) == 0:  #con la funcion len q nos ayuda a buscar 
         return None, None 
 
-    minimum = maximum = array[0]  
+    minimum = maximum = array[0]  #aqui para guardar el array ya ordenado 
     
     for element in array:
         if element < minimum:
@@ -13,5 +13,5 @@ def find_max_min(array):
 
 array = [3, 1, 9, 7, 5, 9]
 max1, min1 = find_max_min(array)
-print("Array - Max =", max1, ", Min =", min1)
+print("Array - Max =", max1, ", Min =", min1) #imprimimos el minimo y el maximo de nuestro array 
 
--- actividad2/selectionSort.py
+++ actividad2/selectionSort.py
@@ -1,16 +1,15 @@
 def selection_sort(arr):
     n = len(arr)
     for i in range(n):
-        min_idx = i  # Inicializamos el índice del mínimo como el índice actual
-        for j in range(i + 1, n):  # Buscamos el mínimo en el subarreglo no ordenado
-            if arr[j] < arr[min_idx]:  # Si encontramos un elemento menor que el mínimo actual
-                min_idx = j  # Actualizamos el índice del mínimo
-        arr[i], arr[min_idx] = arr[min_idx], arr[i]  # Intercambiamos el mínimo con el primer elemento del subarreglo no ordenado
+        min_idx = i  # se inica en le minimo
+        for j in range(i + 1, n):  # Buscamos el mínimo 
+            if arr[j] < arr[min_idx]: 
+                min_idx = j 
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]  # va comparando uno por uno para q se ordene 
     return arr
 
-# Ejemplo de uso:
 entrada = [64, 34, 25, 12, 22, 11, 90]
 salida_selection = selection_sort(entrada.copy())
 
 print("Entrada:", entrada)
-print("Salida Selection Sort:", salida_selection)
+print("Salida Selection Sort:", salida_selection) #imprimimos el arreglo ordenado 
